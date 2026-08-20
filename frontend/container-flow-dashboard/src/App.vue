c4d0fe5b7f4cf82dd46f1d1d83c782759098c153
faza76 <fazakbar76@gmail.com>
2026-08-20 23:12:24 +0700

    frontend: initial file

---
+++ frontend/container-flow-dashboard/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const auth = useAuthStore()
+const router = useRouter()
+
+onMounted(() => {
+  auth.loadFromStorage()
+  if (!auth.isAuthenticated && router.currentRoute.value.name !== 'login') {
+    router.push('/login')
+  }
+})
+</script>
+
+<template>
+  <router-view />
+</template>
